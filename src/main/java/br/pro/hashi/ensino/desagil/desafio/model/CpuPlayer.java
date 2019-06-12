1cc667f1353a4f4ae3c4855f73d099a505937482
Pedro Fontes <pvfontes1000@gmailcom>
2019-06-12 14:19:30 -0300

    delta1 feita

--- src/main/java/br/pro/hashi/ensino/desagil/desafio/model/CpuPlayer.java
+++ src/main/java/br/pro/hashi/ensino/desagil/desafio/model/CpuPlayer.java
@@ -3,64 +3,73 @@ package br.pro.hashi.ensino.desagil.desafio.model;
 import java.util.Stack;
 
 public class CpuPlayer extends Player {
-                                    boolean[][] visited;
-    int numRows;
-                    int numCols;
 
-                    Stack<Node> stack;
+    private final boolean[][] visited;
+    private final int numRows;
+    private final int numCols;
+    private final Stack<Node> stack;
 
 
     CpuPlayer(int row, int col, Board board) {
-                    super(row, col, board);
 
-        numRows = board.getNumRows();
-                        numCols = board.getNumCols();
-
-                                        visited = new boolean[numRows][numCols];
+        super(row, col, board);
+        this.numRows = board.getNumRows();
+        this.numCols = board.getNumCols();
+        this.visited = new boolean[numRows][numCols];
 
         for (int i = 0; i < numRows; i++) {
-                    for (int j = 0; j < numCols; j++) {
-                    visited[i][j] = board.isWall(i, j);
- }
+
+            for (int j = 0; j < numCols; j++) {
+                visited[i][j] = board.isWall(i, j);
+            }
         }
 
-        stack = new Stack<>();
-
-                save();
+        this.stack = new Stack<>();
+        save();
 }
 
 
-                            void move() {
+    public void move() {
+
         if (!stack.isEmpty()) {
-                        if (row > 0 && !visited[row - 1][col]) {
-                        move(-1, 0);
-   save();
-            } else if (col < numCols - 1 && !visited[row][col + 1]) {
+            if (row > 0 && !visited[row - 1][col]) {
+                move(-1, 0);
+                save();
+            }
+
+            else if (col < numCols - 1 && !visited[row][col + 1]) {
                 move(0, 1);
-                                    save(); } else if (row < numRows - 1 && !visited[row + 1][col]) {
+                save();
+            }
+
+            else if (row < numRows - 1 && !visited[row + 1][col]) {
                 move(1, 0);
                 save();
             }
-                        else if (col > 0 && !visited[row][col - 1]) {
+
+            else if (col > 0 && !visited[row][col - 1]) {
                 move(0, -1);
                 save();
-            } else {
+            }
+
+            else {
                 stack.pop();
 
-                                if (!stack.isEmpty()) {
-                                Node node = stack.peek();
+                if (!stack.isEmpty()) {
+                    Node node = stack.peek();
 
-                                row = node.getRow();
-                                col = node.getCol();
-                                }
+                    row = node.getRow();
+                    col = node.getCol();
+
+                }
             }
         }
     }
 
-                    void save() {
-                        visited[row][col] = true;
-                        stack.push(new Node(row, col));
-                    }
+    private void save() {
+        visited[row][col] = true;
+        stack.push(new Node(row, col));
+    }
 
 
     private class Node {
--- src/main/java/br/pro/hashi/ensino/desagil/desafio/model/Target.java
+++ src/main/java/br/pro/hashi/ensino/desagil/desafio/model/Target.java
@@ -1,10 +1,41 @@
 package br.pro.hashi.ensino.desagil.desafio.model;
 
+import java.util.Random;
+
 public class Target extends Element {
-    private final Board board;
+    private Board board;
 
     public Target(int row, int col, Board board) {
         super(row, col);
         this.board = board;
+    }
+
+    public void move(int rowShift, int colShift) {
+        row += rowShift;
+        col += colShift;
+    }
+
+    public void moveRandom() {
+
+        Random movimento = new Random();
+
+        int n = movimento.nextInt(4);
+
+        if (n == 0 && row > 0 && !board.isWall(row - 1, col)) {
+            move(-1, 0);
+        }
+
+        if (n == 1 && col < board.getNumCols() - 1 && !board.isWall(row, col + 1)) {
+            move(0, 1);
+        }
+
+        if (n == 2 && row < board.getNumRows() - 1 && !board.isWall(row + 1, col)) {
+            move(1, 0);
+        }
+
+        if (n == 3 && col > 0 && !board.isWall(row, col - 1)) {
+            move(0, -1);
+        }
+
     }
 }
